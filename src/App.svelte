9220c88573d0a972a589ff174dd8e92f17578b72
georgios.daskalakis <georgios.daskalakis@eurodyn.com>
2024-02-06 10:20:54 +0200

    Init

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import Header from "./lib/Header.svelte";
+    import AddWorkout from "./lib/AddWorkout.svelte";
+
+</script>
+
+<main>
+  <Header></Header>
+  <AddWorkout></AddWorkout>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"];
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let today = new Date();
+    let year = today.getFullYear();
+    let day = days[today.getDay()];
+    let month = months[today.getMonth()];
+</script>
+
+<header class="header">
+    <span class="day">{day}</span>
+    <span class="month">{month}</span>
+    <span class="year">{year}</span>
+</header>
+
+<style>
+    .header {
+        background-color: black;
+        color: white;
+        padding: 10px;
+    }
+
+    .day {
+        font-size: 24px;
+        font-weight: bold;
+    }
+
+    .month {
+        font-size: 24px;
+    }
+</style>
